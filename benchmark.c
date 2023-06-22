d37e4950f9ac8b037c7c743088943be1cd7cba87
returnexitsuccess <returnexitsuccess@gmail.com>
2023-06-22 13:58:17 -0700

    Added numpy times to benchmark

--- benchmark.c
+++ benchmark.c
@@ -14,32 +14,32 @@ int main() {
 
     printf("# Benchmarks\n\n");
 
-    printf("| Test                                     | Time Taken |\n");
-    printf("| ---------------------------------------- | ---------- |\n");
+    printf("| Test                                     | Time Taken | NumPy     |\n");
+    printf("| ---------------------------------------- | ---------- | --------- |\n");
 
     t = benchmarkMultiply(10, 1000000);
-    printf("| 10x10 multiply, 1,000,000 iterations     | %fs  |\n", t);
+    printf("| 10x10 multiply, 1,000,000 iterations     | %fs  | 0.574449s |\n", t);
 
     t = benchmarkMultiply(100, 1000);
-    printf("| 100x100 multiply, 1,000 iterations       | %fs  |\n", t);
+    printf("| 100x100 multiply, 1,000 iterations       | %fs  | 0.042026s |\n", t);
 
     t = benchmarkMultiply(1000, 1);
-    printf("| 1000x1000 multiply, 1 iteration          | %fs  |\n", t);
+    printf("| 1000x1000 multiply, 1 iteration          | %fs  | 0.035474s |\n", t);
 
     t = benchmarkStrassenMultiply(1000, 1);
-    printf("| 1000x1000 strassen multiply, 1 iteration | %fs  |\n", t);
+    printf("| 1000x1000 strassen multiply, 1 iteration | %fs  | 0.035474s |\n", t);
 
     t = benchmarkDeterminant(100, 1000);
-    printf("| 100x100 determinant, 1,000 iterations    | %fs  |\n", t);
+    printf("| 100x100 determinant, 1,000 iterations    | %fs  | 0.046177s |\n", t);
 
     t = benchmarkDeterminant(1000, 1);
-    printf("| 1000x1000 determinant, 1 iteration       | %fs  |\n", t);
+    printf("| 1000x1000 determinant, 1 iteration       | %fs  | 0.014904s |\n", t);
 
     t = benchmarkInverse(100, 1000);
-    printf("| 100x100 inverse, 1,000 iterations        | %fs  |\n", t);
+    printf("| 100x100 inverse, 1,000 iterations        | %fs  | 0.131105s |\n", t);
 
     t = benchmarkInverse(1000, 1);
-    printf("| 1000x1000 inverse, 1 iteration           | %fs  |\n", t);
+    printf("| 1000x1000 inverse, 1 iteration           | %fs  | 0.051880s |\n", t);
 
     return 0;
 }
--- benchmark.md
+++ benchmark.md
@@ -1,12 +1,12 @@
 # Benchmarks
 
-| Test                                     | Time Taken |
-| ---------------------------------------- | ---------- |
-| 10x10 multiply, 1,000,000 iterations     | 0.400279s  |
-| 100x100 multiply, 1,000 iterations       | 0.528280s  |
-| 1000x1000 multiply, 1 iteration          | 0.661665s  |
-| 1000x1000 strassen multiply, 1 iteration | 0.389161s  |
-| 100x100 determinant, 1,000 iterations    | 0.116398s  |
-| 1000x1000 determinant, 1 iteration       | 0.112903s  |
-| 100x100 inverse, 1,000 iterations        | 0.319296s  |
-| 1000x1000 inverse, 1 iteration           | 0.314757s  |
+| Test                                     | Time Taken | NumPy     |
+| ---------------------------------------- | ---------- | --------- |
+| 10x10 multiply, 1,000,000 iterations     | 0.403451s  | 0.574449s |
+| 100x100 multiply, 1,000 iterations       | 0.527973s  | 0.042026s |
+| 1000x1000 multiply, 1 iteration          | 0.664094s  | 0.035474s |
+| 1000x1000 strassen multiply, 1 iteration | 0.392258s  | 0.035474s |
+| 100x100 determinant, 1,000 iterations    | 0.137948s  | 0.046177s |
+| 1000x1000 determinant, 1 iteration       | 0.117739s  | 0.014904s |
+| 100x100 inverse, 1,000 iterations        | 0.313774s  | 0.131105s |
+| 1000x1000 inverse, 1 iteration           | 0.322295s  | 0.051880s |
